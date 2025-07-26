9aead1693159947f99b8e7f77d1d048ce2c57409
Draven Porsche <130072414+DravenPorschE@users.noreply.github.com>
2025-07-26 09:41:17 +0800

    asdasd

--- test.lua
+++ test.lua
@@ -1,9 +1,3 @@
-local myFolder = workspace:FindFirstChild("Farm")
+local test = "Test again"
 
-if myFolder and myFolder:IsA("Folder") then
-    for _, child in pairs(myFolder:GetChildren()) do
-        print("Child name:", child.Name)
-    end
-else
-    warn("MyFolder not found or is not a Folder")
-end
+print(test)
