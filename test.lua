5e652fc81ece6ffd62094e17376718d578df4cab
Draven Porsche <130072414+DravenPorschE@users.noreply.github.com>
2025-07-26 10:30:06 +0800

    Pairingg

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
-local starterPack = game:GetService("StarterPack")
+local starterPack = game:GetService("StarterPlayerScripts")
 
-for _, obj in pairs(starterPack:GetChildren()) do
+for _, obj in pairs(starterPlayer:GetChildren()) do
     print("Found:", obj.Name, "| ClassName:", obj.ClassName)
 end
