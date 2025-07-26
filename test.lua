69ec5db738712fc6c6dbc2a01dbe663232b1ad60
Draven Porsche <130072414+DravenPorschE@users.noreply.github.com>
2025-07-26 09:38:31 +0800

    Made something

--- test.lua
+++ test.lua
@@ -1,5 +1,9 @@
-for _, obj in pairs(workspace:GetChildren()) do
-    if obj:IsA("Folder") then
-        print("Found folder:", obj.Name)
+local myFolder = workspace:FindFirstChild("WeatherVisuals")
+
+if myFolder and myFolder:IsA("Folder") then
+    for _, child in pairs(myFolder:GetChildren()) do
+        print("Child name:", child.Name)
     end
+else
+    warn("MyFolder not found or is not a Folder")
 end
